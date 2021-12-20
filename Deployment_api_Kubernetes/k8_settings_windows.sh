58e2b5ab19e3d030da8453e68e75bea3735c49e8
lassilias <92734819+lassilias@users.noreply.github.com>
2021-12-20 14:01:38 +0100

    Update k8_settings_windows.sh

--- Deployment_api_Kubernetes/k8_settings_windows.sh
+++ Deployment_api_Kubernetes/k8_settings_windows.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+#kubectl delete namespaces ingress-nginx
+export DOCKER_HOST=tcp://127.0.0.1:2375
 minikube start
-# https://medium.com/swlh/how-to-run-locally-built-docker-images-in-kubernetes-b28fbc32cc1d
-# To point your shell to minikube’s docker-daemon, run: 
+#sleep(60)
 eval $(minikube -p minikube docker-env)
-# with this command we run locally built images in Kubernetes, without publishing them to a global registry.
 cd docker_container_api
 docker image build . -t api_churn:latest
 cd ..
-#kubectl delete namespaces ingress-nginx
-export DOCKER_HOST=tcp://127.0.0.1:2375
+#sleep(60)
+#export DOCKER_HOST=tcp://127.0.0.1:2375
 myNamespace="ingress-nginx"
 kubectl get namespace | grep -q "^$myNamespace " || kubectl apply -f config-ingress.yaml
 kubectl config set-context --current --namespace=ingress-nginx
@@ -20,4 +20,3 @@ kubectl create -f my-ingress-project.yml
 #kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
 minikube service  my-api-project-churn-service -n ingress-nginx
 #minikube service my-api-project-churn-deployement --url -n ingress-nginx
-#minikube dashboard --url=True
